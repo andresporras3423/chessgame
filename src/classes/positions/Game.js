88b11161b1bf2ad0cd516a1b05dc657b5dd40237
andresporras3423 <oscarrussi@outlook.com>
2022-03-31 18:46:35 -0500

    add functions for Game class

--- src/classes/positions/Game.js
+++ src/classes/positions/Game.js
@@ -1,15 +1,88 @@
-class Game{
-  constructor(computerSymbol, humanSymbol){
-    this.computerSymbol = computerSymbol;
-    this.humanSymbol = humanSymbol;
-    this.sols= [{"coords": [[0,0],[0,1],[0,2]], "x": "x_row", "o": "o_row"},
-    {"coords": [[1,0],[1,1],[1,2]], "x": "x_row", "o": "o_row"},
-    {"coords": [[2,0],[2,1],[2,2]], "x": "x_row", "o": "o_row"},
-    {"coords": [[0,0],[1,0],[2,0]], "x": "x_column", "o": "o_column"},
-    {"coords": [[0,1],[1,1],[2,1]], "x": "x_column", "o": "o_column"},
-    {"coords": [[0,2],[1,2],[2,2]], "x": "x_column", "o": "o_column"},
-    {"coords": [[0,0],[1,1],[2,2]], "x": "x_diagonal1", "o": "o_diagonal1"},
-    {"coords": [[0,2],[1,1],[2,0]], "x": "x_diagonal2", "o": "o_diagonal2"}]
-  }
+import Positions from "./Positions";
+import BoardData from "./BoardData";
+
+class Game {
+  constructor() {}
+
+  start_game = () => {
+    this.positions = new Positions();
+    this.moves_done = 0;
+    this.boardData = {};
+    this.positions.set_initial_board();
+    this.next_white_move();
+  };
+
+  next_white_move = () => {
+    let movements = Array.from(this.positions.available_white_moves());
+    if (this.is_it_game_over(movements.length)) return;
+    this.add_recent_board(movements.length);
+    this.print_last_board_info();
+    let rnd = Math.floor(Math.random() * movements.length);
+    let last_movement = movements[rnd];
+    this.positions.update_board_details_after_white_move(last_movement);
+    this.positions.set_initial_board();
+    this.next_black_move();
+  };
+
+  next_black_move = () => {
+    let movements = Array.from(this.positions.available_black_moves());
+    if (this.is_it_game_over(movements.length)) return;
+    this.add_recent_board(movements.length);
+    this.print_last_board_info();
+    let rnd = Math.floor(Math.random() * movements.length);
+    let last_movement = movements[rnd];
+    this.positions.update_board_details_after_black_move(last_movement);
+    this.positions.set_initial_board();
+    this.next_white_move();
+  };
+
+  give_current_board = () => {
+    this.current_board = "";
+    this.positions.cells.forEach((row, i) => {
+      row.forEach((cell, j) => {
+        this.current_board += cell.substring(0, 2);
+        if (j < 7) this.current_board += ",";
+      });
+      if (i < 7) current_board += "*";
+    });
+    this.current_board;
+  };
+
+  is_it_game_over = (total_movements) => {
+    if (
+      total_movements == 0 ||
+      this.positions.black_pieces.keys.length +
+        this.positions.white_pieces.keys.length ==
+        2 ||
+      this.positions.checkmate_still_possible == false
+    ) {
+      if (total_movements == 0) add_recent_board(total_movements);
+      return true;
+    }
+    return false;
+  };
+
+  add_recent_board = (total_movements) => {
+    this.board = new BoardData(give_current_board,
+                             this.positions.black_pieces.keys.length,
+                             this.positions.white_pieces.keys.length,
+                             this.positions.black_long_castling,
+                             this.positions.black_short_castling,
+                             this.positions.white_long_castling,
+                             this.positions.white_short_castling,
+                             last_movement_reduced,
+                             total_movements,
+                             [",", "b"].includes(last_movement_reduced[0]) ? "white" : "black");
+  };
+
+  last_movement_reduced = () => {
+    return this.positions.last_movement.split(",", -1).map(pos=> pos.substring(0,2)).join(",");
+  };
+
+  print_last_board_info = () => {
+    this.moves_done++;
+    console.log(`turn: ${(this.moves_done + 1) / 2}`);
+    console.log(this.board.print_info);
+  };
 }
 export default Game;
